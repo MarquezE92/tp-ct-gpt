553003d0063b6a80871a92b8863a155791ecae2b
MarquezE92 <vsuykyasmbp@hotmail.com>
2023-06-18 22:29:35 -0300

    first exercise - part 2

--- 02 - Code Quality/ejercicio1.js
+++ 02 - Code Quality/ejercicio1.js
@@ -1,3 +1,5 @@
+/*    before improving it with Chat GPT
+
 function complicatedLevelOne(arr) {
    var result = 0;
 
@@ -13,5 +15,28 @@ function complicatedLevelOne(arr) {
 
    return result;
 }
+ */
 
-console.log(complicatedLevelOne([3, 5, 2, 6, 3, 5]));
+/**
+ * Calcula la suma de los elementos de un array con una condición especial.
+ *
+ * @param {number[]} arr - Array de números.
+ * @returns {number} - Suma resultante con la condición aplicada.
+ */
+function calculateSumWithCondition(arr) {
+   let result = 0;
+
+   for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+
+      if (arr[i] % 2 === 0) {
+         result *= i;
+      } else {
+         result -= i;
+      }
+   }
+
+   return result;
+}
+
+console.log(calculateSumWithCondition([3, 5, 2, 6, 3, 5]));
