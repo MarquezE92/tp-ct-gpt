7e20992961f42858717be01234fc90f7552c335e
MarquezE92 <vsuykyasmbp@hotmail.com>
2023-06-18 23:16:07 -0300

    second and third exercise - part 2

--- 02 - Code Quality/ejercicio2.js
+++ 02 - Code Quality/ejercicio2.js
@@ -1,3 +1,5 @@
+/*     before improving it with Chat GPT 
+
 function complicatedLevelTwo(array) {
    var result = array
       .map((x) => x * 2)
@@ -20,7 +22,37 @@ function complicatedLevelTwo(array) {
       var str = 'The final result is: ' + result;
       return str;
    }
+} */
+
+/**
+ * Calcula un resultado complicado basado en un array de números.
+ *
+ * @param {number[]} numbers - Array de números.
+ * @returns {string|number} - El resultado calculado.
+ */
+function calculateComplicatedResult(numbers) {
+   let multipliedNumbers = numbers.map((number) => number * 2);
+   let divisibleByThree = multipliedNumbers.filter((number) => number % 3 === 0);
+   let sum = divisibleByThree.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+   if (sum > 1000) {
+      let temp = sum;
+      for (let i = 0; i < 10; i++) {
+         temp -= i;
+         temp *= 2;
+      }
+      return temp;
+   } else {
+      let count = 0;
+      while (count < 3) {
+         sum += count;
+         count++;
+      }
+      let resultString = 'El resultado final es: ' + sum;
+      return resultString;
+   }
 }
 
+
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(complicatedLevelTwo(array));
+console.log(calculateComplicatedResult(array));
--- 02 - Code Quality/ejercicio3.js
+++ 02 - Code Quality/ejercicio3.js
@@ -1,3 +1,5 @@
+/*  before improving it with Chat GPT 
+
 function complicatedLevelThree(obj) {
    var result = '';
    var keys = Object.keys(obj);
@@ -29,7 +31,50 @@ function complicatedLevelThree(obj) {
    var formattedResult = reversedResult.toUpperCase();
 
    return formattedResult;
+} */
+
+
+/**
+ * Calcula un resultado complicado basado en un objeto.
+ *
+ * @param {Object} obj - Objeto con claves y valores.
+ * @returns {string} - El resultado calculado en formato de cadena en mayúsculas.
+ * @throws {TypeError} - Si el parámetro `obj` no es un objeto válido.
+ */
+function calculateComplicatedResult(obj) {
+   if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+      throw new TypeError('El parámetro `obj` debe ser un objeto válido.');
+   }
+
+   let result = '';
+   let keys = Object.keys(obj);
+   let values = Object.values(obj);
+   let sum = values.reduce((acc, curr) => acc + curr, 0);
+
+   for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+      let value = values[i];
+      if (value % 2 === 0) {
+         result += key + ' is even. ';
+      } else {
+         for (let temp = value; temp > 0; temp--) {
+            result += key + ', ';
+         }
+      }
+   }
+
+   let finalResult = '';
+   let count = 0;
+   while (count < sum) {
+      finalResult += result;
+      count++;
+   }
+
+   let formattedResult = finalResult.split('').reverse().join('').toUpperCase();
+
+   return formattedResult;
 }
+
 
 var obj = {
    a: 2,
@@ -38,4 +83,4 @@ var obj = {
    d: 5,
 };
 
-console.log(complicatedLevelThree(obj));
+console.log(calculateComplicatedResult(obj));
